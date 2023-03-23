c1216de5738e12df75f11f33088bf2553466d2c0
aowen7 <122053762+aowen7@users.noreply.github.com>
2023-03-23 12:03:14 -0700

    initial commit
    trigger game over when good objects hit the sensor

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,7 +19,7 @@ public class GameManager : MonoBehaviour
         score = 0;
         UpdateScore(0);
 
-        gameOverText.gameObject.SetActive(true);
+        
     }
 
     // Update is called once per frame
@@ -43,5 +43,10 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
     }
 }
