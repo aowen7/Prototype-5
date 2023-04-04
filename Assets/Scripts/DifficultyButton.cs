0f703c3ab2a2f52af821175ae0635f5c1370602f
aowen7 <122053762+aowen7@users.noreply.github.com>
2023-04-04 10:04:28 -0700

    initial commit
    start game on clicking a button

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class DifficultyButton : MonoBehaviour
 {
     private Button button;
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
         button = GetComponent<Button>();
         button.onClick.AddListener(SetDifficulty);
+
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -23,5 +26,7 @@ public class DifficultyButton : MonoBehaviour
     void SetDifficulty()
     {
         Debug.Log(gameObject.name + " was clicked");
+
+        gameManager.StartGame();
     }
 }
